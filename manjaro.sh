365067f1e0ba721549aad952aaaa5d56bfcd1844
Muhammet Atilla <muhammetcanaltan14@gmail.com>
2025-02-17 18:08:49 +0300

    Create manjaro.sh

---
+++ manjaro.sh
@@ -0,0 +1,16 @@
+yellow='\e[0;33m'
+
+echo -e "$yellow" Entered By System
+clear
+figlet "HackMan"
+echo "HackMan Tool"
+echo "Tool Created by babarich456"
+
+read -p "Are you need? (y/n) " choice
+clear
+echo -e "$yellow" Manjaro is being run from Termux:
+pkg update && pkg upgrade
+pkg install proot wget
+wget https://raw.githubusercontent.com/sdrausty/TermuxArch/master/setupTermuxArch.sh
+bash setupTermuxArch.sh
+startarch
