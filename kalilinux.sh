f704bbb4ff0b2a520d3f4eee7204384901904bb0
Muhammet Atilla <muhammetcanaltan14@gmail.com>
2025-02-17 17:57:06 +0300

    Create kalilinux.sh

---
+++ kalilinux.sh
@@ -0,0 +1,19 @@
+yellow='\e[0;33m'
+
+echo -e "$yellow" Entered By System
+clear
+figlet "HackMan"
+echo "HackMan Tool"
+echo "Tool Created by babarich456"
+
+read -p "Enter your choice : " choice
+clear
+echo -e "$yellow" Kali Linux is being run from Termux:
+echo "If there is no "nh", please type "nethunter"."
+apt update
+apt upgrade
+apt install wget
+wget -O install-nethunter-termux https://offs.ec/2MceZWr
+chmod +x install-nethunter-termux
+./install-nethunter-termux
+nh
