d63a86bf1f390e59609ff819b5573d2ac3483286
Muhammet Atilla <muhammetcanaltan14@gmail.com>
2025-02-18 15:41:10 +0300

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,6 +1,6 @@
-yellow='\e[0;33m'
+green='\033[92m'
 
-echo -e "$yellow" Entered By System
+echo -e "$green" Entered By System
 clear
 figlet "HackMan"
 echo "HackMan Tool"
@@ -8,7 +8,7 @@ echo "Tool Created by babarich456"
 echo 
 read -p "Are you need? (y/n) " choice
 clear
-echo -e "$yellow" Ubuntu is being run from Termux:
+echo -e "$green" Ubuntu is being run from Termux:
 pkg update && pkg upgrade
 pkg install curl
 curl -fsSLO https://raw.githubusercontent.com/jorexdeveloper/termux-ubuntu/main/install-ubuntu.sh
