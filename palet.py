add453d4faa21ca3cd011be3f8b80a14c60c2697
Muhammet Atilla <muhammetcanaltan14@gmail.com>
2025-02-18 17:04:26 +0300

    Update palet.py

--- palet.py
+++ palet.py
@@ -1,38 +1,59 @@
-Sure, here is the equivalent Python code:
-
-```python
+#!/usr/bin/python3
+#Copyright 2021 AllHackingTools
+#Written by : Misha Korzhik
+#Github     : http://github.com/mishakorzik                                                                           
 import os
-import system
-import subprocess
+import time
+import sys
 
-# Define the green color code
-green = '\033[92m'
+os.system("clear")
+os.system("cd && cd Lernux")
+os.system("bash main.sh")
 
-# Print the login message in green
-print(f"{green}Logged into the system")
-os.system('clear')
-os.system('figlet "Lernux"')
-print("Lernux Tool")
-print("Tool Created by babarich456")
-
-# Ask the user if they need something
-choice = input("Are you need? (y/n) ")
-os.system('clear')
-print(f"{green}Kali Linux is being run from Termux:")
-print("If there is no \"nh\", please type \"nethunter\".")
-
-# Update and upgrade the package list
-subprocess.run(['apt', 'update'])
-subprocess.run(['apt', 'upgrade'])
-
-# Install wget
-subprocess.run(['apt', 'install', 'wget'])
-
-# Download and run the install-nethunter-termux script
-subprocess.run(['wget', '-O', 'install-nethunter-termux', 'https://offs.ec/2MceZWr'])
-os.chmod('install-nethunter-termux', 0o755)
-subprocess.run(['./install-nethunter-termux'])
-
-# Run nh and nethunter commands
-subprocess.run(['nh'])
-subprocess.run(['nethunter'])
+op = str(input("Options: "))
+if op == "1":
+ os.system("Arch Linux (bash archlinux.sh)")
+ time.sleep(0.3)
+ os.system("cd && cd Lernux && bash archlinux.sh")
+elif op == "2":
+ os.system("Kali Linux (bash kalilinux.sh)")
+ time.sleep(0.3)
+ os.system("cd && cd Lernux && bash kalilinux.sh")
+elif op == "3":
+ os.system("Debian (bash debian.sh)")
+ time.sleep(0.3)
+ os.system("cd && cd Lernux && bash debian.sh")
+elif op == "4":
+ os.system("Fedora (bash fedora.sh)")
+ time.sleep(0.3)
+ os.system("cd && cd Lernux && bash fedora.sh")
+elif op == "5":
+ os.system("Gentoo (bash gentoo.sh)")
+ time.sleep(0.3)
+ os.system("cd && cd Lernux && bash gentoo.sh")
+elif op == "6":
+ os.system("Kali Linux (bash kalilinux.sh)")
+ time.sleep(0.3)
+ os.system("cd && cd Lernux && bash kalilinux.sh")
+elif op == "7":
+ os.system("Ubuntu (bash ubuntu.sh)"")
+ time.sleep(0.3)
+ os.system("cd && cd lernux && bash ubuntu.sh")
+elif op == "8":
+  elif op == "13324715":
+ print("[DEBUG] Developer mode successfully enabled!")
+ time.sleep(0.8)
+ os.system("cd && cd Lernux && cd .settings && mv palet.py /data/data/com.termux/files/home/Lernux/.temp/ && mv palet.py /data/data/com.termux/files/home/Lernux/.temp/")
+ print("[DEBUG] Please restart Lernux!")
+ os.system("cd && cd Lernux && mv palet.py /data/data/com.termux/files/home/Lernux/.temp/temp && cd .settings && cd debug && cp palet.py /data/data/com.termux/files/home/Lernux/")
+ print("[DEBUG] Warning! Customization has been disabled.")
+elif op == "24":
+ os.system("clear && cd && cd Lernux && bash main.sh")
+ print("\033[1;31;40mExiting System...")
+ time.sleep(0.7)
+else:
+ print("\033[1;31;40mInvalid input. Reloading Tools")
+ time.sleep(1.6)
+ os.system("cd")
+ os.system("cd Lernux")
+ os.system("python2 palet.py")
