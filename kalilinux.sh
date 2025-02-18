cc41ec492d437fd922fe07c0c5b526e2b54e6c71
Muhammet Atilla <muhammetcanaltan14@gmail.com>
2025-02-18 15:49:21 +0300

    Update kalilinux.sh

--- kalilinux.sh
+++ kalilinux.sh
@@ -1,6 +1,6 @@
-yellow='\e[0;33m'
+green='\033[92m'
 
-echo -e "$yellow" Entered By System
+echo -e "$green" Logged into the system
 clear
 figlet "HackMan"
 echo "HackMan Tool"
@@ -8,7 +8,7 @@ echo "Tool Created by babarich456"
 
 read -p "Are you need? (y/n) " choice
 clear
-echo -e "$yellow" Kali Linux is being run from Termux:
+echo -e "$green" Kali Linux is being run from Termux:
 echo "If there is no "nh", please type "nethunter"."
 apt update
 apt upgrade
