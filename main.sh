10fe41cd2f502a32310bf02ad237e97cd097504d
Muhammet Atilla <muhammetcanaltan14@gmail.com>
2025-02-18 16:16:21 +0300

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,24 @@
+green='\033[92m'
+
+echo -e "$green" Logged into the system
+clear
+figlet "Lernux"
+echo "Lernux Tool"
+echo "Tool Created by babarich456"
+
+read -p "Do you want to enter the tool? (y/n) " choice
+echo
+clear
+echo -e "$green" Logged into the system.
+figlet "Lernux"
+echo "Kali Linux Distributions: "
+echo "Arch Linux (bash archlinux.sh)"
+echo "Debian (bash debian.sh)"
+echo "Fedora (bash fedora.sh)"
+echo "Gentoo (bash gentoo.sh)"
+echo "Kali Linux (bash kalilinux.sh)"
+echo "Manjaro (bash manjaro.sh)"
+echo "Slackware (bash slackware.sh)"
+echo "Ubuntu (bash ubuntu.sh)"
+echo "Example: "
+echo "bash request.sh"
