102851ac2894305d961177a8b0f220d02de9bd08
Muhammet Atilla <muhammetcanaltan14@gmail.com>
2025-02-18 15:52:43 +0300

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,6 +1,6 @@
-yellow='\e[0;33m'
+green='\033[92m'
 
-echo -e "$yellow" Entered By System
+echo -e "$green" Entered By System
 clear
 figlet "HackMan"
 echo "HackMan Tool"
@@ -8,7 +8,7 @@ echo "Tool Created by babarich456"
 
 read -p "Are you need? (y/n) " choice
 clear
-echo -e "$yellow" Debian is being run from Termux:
+echo -e "$green" Debian is being run from Termux:
 pkg update && pkg upgrade
 pkg install proot-distro
 proot-distro install debian
